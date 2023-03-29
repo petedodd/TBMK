afd4c15acbc13447932b22cc316632967a96f972
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2023-03-29 14:07:27 +0100

    prepping data for use in model

---
+++ modelling/modeldata.R
@@ -0,0 +1,331 @@
+## === preparing data ===
+# using meta-analyses for TBM in kids to get numbers
+library(here)
+library(glue)
+library(data.table)
+library(ggplot2)
+library(ggthemes)
+library(scales)
+library(countrycode)
+library(readxl)
+id <- 'modelling/indata/'
+dd <- 'modelling/data/'
+xd <- 'modelling/plots/'
+md <- 'metaanalyses/data/'
+mp <- 'metaanalyses/plots/'
+
+## wdr <- '~/Documents/gtb2021/'
+## there <- function(x) glue(wdr) + x
+ssum <- function(x) sqrt(sum(x^2)) ## summaries
+gh <- function(x) glue(here(x))
+S <- function(...)sum(...,na.rm=TRUE)
+## S(1,2,3)
+## S(1,NA,3)
+
+
+
+## --- data from meta-analyses
+## proportion of TBM in notifications by age
+load(gh('{mp}BS.Rdata')) #
+BS
+
+## proportion of notifications in each age group
+load(gh('{mp}AP.Rdata')) #
+AP
+
+## --- WHO data
+## notification data in children by country
+## WHO load last year's estimates
+## https://www.who.int/teams/global-tuberculosis-programme/data
+
+## age-stratified incidence
+IS <- fread(gh('{id}TB_burden_age_sex_2023-03-29.csv'))
+IS[,se:=(hi-lo)/3.92]
+IS <- IS[age_group %in% c('0-4','5-14'),
+         .(inc.age=sum(best),inc.age.sd=ssum(se)),
+         by=.(iso3,age_group)] #sum over sex
+IS[,age_group:=gsub('-','_',age_group)]
+ESA <- copy(IS)
+
+## notifications
+tb <- fread(gh('{id}TB_notifications_2023-03-29.csv'))
+nnmz <- c('n.0.4','n.5.9','n.10.14','n.5.14','n.0.14')
+
+TBN <- tb[year==max(year)]
+TBN[,(nnmz):=.(S(newrel_m04,newrel_f04,newrel_sexunk04),
+               S(newrel_m59,newrel_f59),
+               S(newrel_m1014,newrel_f1014),
+               S(newrel_m514,newrel_f514,newrel_sexunk514),
+               S(newrel_m014,newrel_f014,newrel_sexunk014)
+               ),
+    by=iso3]
+
+nnmz1 <- c('iso3',nnmz)
+TBN <- TBN[,..nnmz1]
+
+## merge with incidence
+nnmz1 <- c('iso3','n.0.4','n.5.14')
+TBN <- TBN[,..nnmz1]
+names(TBN)[2:3] <- c('0_4','5_14')
+TBN <- melt(TBN,id='iso3')
+names(TBN)[2] <- 'age_group'
+names(TBN)[3] <- 'notes'
+TBN <- merge(TBN,ESA,by=c('iso3','age_group'))
+
+## CDR by country
+TBN[,cdr.mn:=(notes+1)/(inc.age+1)]
+TBN[,cdr.vr:=(cdr.mn*inc.age.sd/(1+inc.age))^2]
+TBN[,cdr.mu:=log(cdr.mn^2/sqrt(cdr.mn^2+cdr.vr))]
+TBN[,cdr.sg:=sqrt(log(1+cdr.vr/cdr.mn^2))]
+sg.m <- TBN[,.(fix=mean(cdr.sg)),by=age_group]
+TBN <- merge(TBN,sg.m,by='age_group',all.x=TRUE)
+TBN[cdr.sg==0,cdr.sg:=fix]
+TBN[,fix:=NULL]
+
+TBN[,cdr.ab:=cdr.mn*(1-cdr.mn)/(cdr.mn*inc.age.sd/(1+inc.age))^2-1] #v=k/U,dv=k.dU/U^2=v.dU/U
+TBN[,c('cdr.a','cdr.b'):= .(cdr.mn * cdr.ab, (1-cdr.mn) * cdr.ab)]
+mn <- TBN[is.finite(cdr.a+cdr.b),.(ab=mean(cdr.ab)),by=age_group]
+mn2 <- TBN[is.finite(cdr.a+cdr.b),.(mn=sum(notes)/sum(inc.age)),by=age_group]
+mn <- merge(mn,mn2,by='age_group')
+mn[,c('a','b'):= .(mn * ab, (1-mn) * ab)]
+TBN <- merge(TBN,mn,by='age_group',all.x=TRUE)
+TBN[!is.finite(cdr.a+cdr.b),c('cdr.a','cdr.b'):=.(a,b)]
+TBN[,c('a','b'):=NULL]
+TBN[,c('cdr.ab','ab','mn'):=NULL]
+
+save(TBN,file=gh('{dd}TBN.Rdata'))
+
+## BCG by country
+## from http://www.who.int/entity/immunization/monitoring_surveillance/data/coverage_estimates_series.xls?ua=1
+BCG <- read_xls(gh('{id}coverage_estimates_series.xls'),sheet="BCG")
+BCG <- as.data.table(BCG)[Vaccine=='BCG',.(iso3=ISO_code,bcg=`2019`)]
+
+save(BCG,file=gh('{dd}BCG.Rdata'))
+
+
+## FOI estimates by country
+P <- fread(gh('{id}IHME-GBD_2019_DATA-043d7d7b-1.csv'))
+## make key
+key <- data.table(ihme=P[,unique(location_name)])
+key[,iso3:=countrycode(ihme,origin = 'country.name',destination = 'iso3c')]
+P <- merge(P,key,by.x = 'location_name',by.y = 'ihme')
+P[,val.sd:=(upper-lower)/3.92]
+P[iso3=='ZAF',val*1e5] #not actually percent
+P <- P[,.(prev=1e5*sum(val),prev.sd=1e5*ssum(val.sd)),by=iso3]
+
+save(P,file=gh('{dd}P.Rdata'))
+
+
+## IHME populations 
+POP <- fread(gh('{id}IHME_GBD_2019_POP_SYA_2019_Y2021M01D28.CSV'))
+POP <- merge(POP,key,by.x='location_name',by.y='ihme',all.x=FALSE)
+lagz <- c('<1 year','1','2','3','4','5','6','7','8','9','10','11','12','13','14')
+lagg <- c('<1',rep('1-4',4),rep('5-9',5),rep('10-14',5))
+akey <- data.table(age_group_name=lagz,acat=lagg)
+
+POP <- POP[sex_name=='both']
+POP <- POP[age_group_name %in% lagz]
+POP <- merge(POP,akey,by='age_group_name',all.x = TRUE)
+POP <- POP[,.(pop=sum(val)),by=.(iso3,acat)]
+
+save(POP,file=gh('{dd}POP.Rdata'))
+
+
+## ============================================
+## --- join and output
+load(file=gh('{dd}TBN.Rdata'))
+load(file=gh('{dd}P.Rdata'))
+load(file=gh('{dd}POP.Rdata'))
+
+## country list
+length(TBN[,unique(iso3)]) #215
+length(POP[,unique(iso3)]) #204
+length(P[,unique(iso3)]) #204
+isoz <- P[,unique(iso3)]
+isoz <- intersect(isoz,TBN[,unique(iso3)])
+length(isoz) #202
+
+## master list
+isoz <- sort(isoz)
+cat(isoz,file=gh('{dd}isoz.txt'))
+
+## notes & CDR
+TBNW <- dcast(data=TBN,
+              formula = iso3 ~ age_group,
+              value.var = c('notes','cdr.mu','cdr.sg'))
+
+## NOTE check beta version
+setkey(TBNW,iso3)
+
+## bcg
+setkey(BCG,iso3)
+BCG <- BCG[isoz]
+bcg.mean <- mean(BCG$bcg,na.rm = TRUE)
+BCG[is.na(bcg),bcg:=bcg.mean]
+
+## pop
+POPW <- dcast(POP,iso3~acat,value.var='pop')
+POPW <- POPW[,.(iso3,`<1`,`1-4`,`5-9`,`10-14`)]
+setkey(POPW,iso3)
+sag <- unique(lagg)
+
+## prev
+setkey(P,iso3)
+
+
+## stan data
+sdata <- list(
+  N = length(isoz),
+  CDR_mu = as.matrix(TBNW[isoz][,.(cdr.mu_0_4,cdr.mu_0_4,cdr.mu_5_14,cdr.mu_5_14)]),
+  CDR_sg = as.matrix(TBNW[isoz][,.(cdr.sg_0_4,cdr.sg_0_4,cdr.sg_5_14,cdr.sg_5_14)]),
+  notif_data = as.matrix(TBNW[isoz][,.(notes_0_4,notes_5_14)]),
+  BCG_coverage = BCG,
+  POPS = as.matrix(POPW[isoz][,..sag]),
+  PREVM = P[isoz][,prev], PREVS = P[isoz][,prev.sd]
+)
+
+## gamma: var=shape/rate^2, mean=shape/rate (sd=sqrt(shape)/rate)
+## shape=(mean/sd)^2, rate = mean/sd^2
+## sdata$PREVk <- (sdata$PREVM/sdata$PREVS)^2
+## sdata$PREVr <- sdata$PREVM/(sdata$PREVS)^2
+
+## summary(sdata$PREVk)
+## summary(sdata$PREVr)
+
+## LN version
+sdata$PREVmu <- log(sdata$PREVM^2/sqrt(sdata$PREVM^2+sdata$PREVS^2))
+sdata$PREVsg <- log(1+(sdata$PREVS/sdata$PREVM)^2)
+summary(sdata$PREVmu)
+summary(sdata$PREVsg)
+
+sdata$PREVM <- sdata$PREVS <- NULL
+
+
+## beta risk
+sdata$mu_styblo <- 1.677891
+sdata$sig_styblo <- 0.3714703
+
+## proportion of note TBM
+sdata$mu_notes <- BS$pred
+sdata$sig_notes <- BS$se
+
+## progression risk to TBM
+## sdata$mu_prog <- BD$pred.l[1:4]
+## sdata$sig_prog <- BD$pred.sdl[1:4]
+
+## ## just cohorts:
+## TODO
+sdata$mu_prog <- BD$prog.mnl[1:4]
+sdata$sig_prog <- BD$prog.sdl[1:4]
+
+## proportion of note in each age
+sdata$mu_age <- AP$pred
+sdata$sig_age <- AP$se
+
+## BCG
+## ## Marais
+## sdata$bcgProtA <- 1.25
+## sdata$bcgProtB <- 2.5
+## mean(rbeta(1e4,1.25,2.5)) #HR
+
+## Bourdin Trunz: 73 67-79
+pab <- HEdtree::getAB(0.73,(0.79-0.67)^2/3.92^2)
+## NOTE swap for HR
+sdata$bcgProtA <- pab$b
+sdata$bcgProtB <- pab$a
+mean(rbeta(1e4,sdata$bcgProtA,sdata$bcgProtB)) #HR
+
+## correct coverate
+sdata$BCG_coverage <- sdata$BCG_coverage$bcg
+
+
+## ## check beta parms
+## summary(sdata$CDR_A)
+## summary(sdata$CDR_B)
+
+## ## u5
+## bad <- (rowSums(sdata$CDR_A[,1:2]<0) + rowSums(sdata$CDR_B[,1:2]<0)) > 0
+## av.a <- sum(sdata$CDR_A[!bad,1])/sum(!bad)
+## av.b <- sum(sdata$CDR_B[!bad,1])/sum(!bad)
+## sdata$CDR_A[bad,1:2] <- av.a
+## sdata$CDR_B[bad,1:2] <- av.b
+
+## ## o5
+## bad <- (rowSums(sdata$CDR_A[,3:4]<0) + rowSums(sdata$CDR_B[,3:4]<0)) > 0
+## av.a <- sum(sdata$CDR_A[!bad,3])/sum(!bad)
+## av.b <- sum(sdata$CDR_B[!bad,3])/sum(!bad)
+## sdata$CDR_A[bad,3:4] <- av.a
+## sdata$CDR_B[bad,3:4] <- av.b
+
+## notification safety
+sdata$notif_data[sdata$notif_data[,1]==0,1] <- 1
+sdata$notif_data[sdata$notif_data[,2]==0,2] <- 1
+
+
+sdata$couple <- 1
+
+
+## region key
+regkey <- unique(tb[,.(iso3,g.whoregion=g_whoregion)])
+setkey(regkey,iso3)
+regkey <- regkey[isoz]
+
+test <- model.matrix(iso3 ~ 0 + g.whoregion,data=regkey)
+rkey <- as.matrix(test)
+head(rkey,n=10)
+
+for(nm in colnames(rkey)){
+  nnm <- gsub("g\\.","",nm)
+  sdata[[nnm]] <- which(rkey[,nm]==1)
+  ## sdata[[nm]] <- NULL
+}
+
+RN <- rep(0,6)
+for(i in 1:6) RN[i] <- sum(rkey[,i])
+
+sdata$RN <- RN
+
+names(sdata)
+
+## Silvia
+## 19·3% (95% CI 14·0–26·1)
+bab <- HEdtree::getAB(0.193,(14.0-26.1)^2/392^2)
+
+CFRtxA <- rep(bab$a,4)
+CFRtxB <- rep(bab$b,4)
+
+sdata$CFRtxA <- CFRtxA
+sdata$CFRtxB <- CFRtxB
+
+## age-dependent ORs
+
+load(gh('{mp}cfrOR.Rdata'))
+
+cfrOR
+sdata$lgOR <- cfrOR$lgOR
+sdata$lgORsd <- cfrOR$lgOR.sd
+
+
+## sdata$mu_age
+## sdata$sig_age
+
+## sequelae from Silvia review
+## 53·9 (42·6–64·9)
+tmp <- HEdtree::getAB(0.539,(64.9-42.6)^2/392^2)
+rtmp <- rbeta(1e4,tmp$a,tmp$b)
+summary(rtmp)
+hist(rtmp)
+sdata$seqA <- rep(tmp$a,4)
+sdata$seqB <- rep(tmp$a,4)
+
+
+## wide notif data
+sdata$notif_dataW <- cbind(sdata$notif_data[,c(1,1)],sdata$notif_data[,c(2,2)])
+
+
+save(sdata,file=gh('{dd}sdata.Rdata'))
+
+## TODO
+## BD
+## HIV ORs x2
+## age split?
---
+++ modelling/stan/combine.stan
@@ -0,0 +1,156 @@
+/* estimate incidence*/
+functions{
+  //apply log OR to probability
+  real alo(real logOR, real p){
+    return inv_logit(logit(p)+logOR);
+  }
+}
+data{
+  int N; //no countries
+  int couple;//couple notifs and progression?
+  real mu_styblo; real<lower=0> sig_styblo; //beta
+  real mu_notes[4]; real<lower=0> sig_notes[4]; //proportion of note TBM
+  real mu_prog[4]; real<lower=0> sig_prog[4]; //progrsesion risk to TBM 
+  real mu_age[2]; real<lower=0> sig_age[2]; //proportion of note in each age
+  real<lower=0> bcgProtA; real<lower=0> bcgProtB; //BCG protection as HR
+  matrix[N,2] notif_data;
+  matrix[N,4] notif_dataW;  
+  matrix[N,4] CDR_mu;   matrix[N,4] CDR_sg; //prior for detection 
+  vector[N] BCG_coverage;
+  matrix[N,4] POPS;
+  vector[N] PREVmu; vector[N] PREVsg; 
+  int RN[6];//
+  int whoregionAFR[RN[1]]; 
+  int whoregionAMR[RN[2]]; 
+  int whoregionEMR[RN[3]]; 
+  int whoregionEUR[RN[4]];
+  int whoregionSEA[RN[5]]; 
+  int whoregionWPR[RN[6]];
+  real seqA[4]; real seqB[4]; //meta-analysis sequelae  
+  real CFRtxA[4]; real CFRtxB[4]; //meta-analysis CFR
+  real lgOR[4]; real lgORsd[4];  //CFR age ORs from ZAF data
+}
+transformed data{
+
+}
+parameters{
+  vector<lower=0>[N] PREV;
+  real<lower=0> styblo;
+  real<lower=0,upper=1> BCGprot;
+  //matrix<lower=0.0001,upper=0.999>[N,4] propTBM;  
+  vector<lower=0.0001,upper=0.999>[4] propTBM;
+  //vector<lower=0,upper=1>[4] propTBM;
+  vector<lower=0.01,upper=0.999>[2] propAge;
+  vector<lower=0,upper=1>[4] prog;
+  real<lower=0> lgcfrOR[4];
+}
+transformed parameters{
+  vector<lower=0>[N] foi;
+  vector<lower=0>[N] tbivec;
+  matrix<lower=0>[N,4] tbimat;
+  vector<lower=0,upper=1>[4] propAgeLong;
+  matrix<lower=0,upper=1>[N,4] propAmat;
+  matrix<lower=0,upper=1>[N,4] propTBMmat;
+  matrix<lower=0>[N,4] TBMnotes;
+  matrix<lower=0>[N,4] TBMI;
+  matrix<lower=0,upper=1>[N,4] progmat;
+  matrix<lower=0>[N,4] NoI;
+  // //force of infection
+  foi = PREV * styblo * 0.5/1e5;
+  propTBMmat = rep_matrix(propTBM',N);
+  propAgeLong[1] = propAge[1];
+  propAgeLong[3] = propAge[2];
+  propAgeLong[2] = 1-propAge[1];
+  propAgeLong[4] = 1-propAge[2];
+  propAmat =  rep_matrix(propAgeLong',N);
+  TBMnotes = propAmat .* propTBMmat .* notif_dataW;
+  tbivec = foi .* (1-(1-BCGprot) * 1e-2 .* BCG_coverage);//NOTE
+  tbimat = rep_matrix(tbivec,4);
+  progmat = rep_matrix(prog',N);
+  TBMI = tbimat .* progmat .* POPS;
+  // //CDR estimate
+  NoI = TBMnotes ./ TBMI;
+}
+model{
+  PREV ~ lognormal(PREVmu,PREVsg);
+  styblo ~ lognormal(mu_styblo,sig_styblo);
+  BCGprot ~ beta(bcgProtA,bcgProtB);
+  propTBM ~ lognormal(mu_notes,sig_notes);//BUG here?(-2,0.1);//
+  propAge ~ lognormal(mu_age,sig_age);//also bad but order less //(-1,0.1)
+  //propAge ~ beta(mu_age,sig_age);//NOTE debug
+  prog ~ lognormal(mu_prog,sig_prog);
+  //CFR ORs
+  lgcfrOR ~ normal(lgOR,lgORsd);
+  if(couple>0){
+      target += lognormal_lpdf( to_vector(NoI) | to_vector(CDR_mu), to_vector(CDR_sg) );
+  }
+}
+generated quantities{
+ //reformed CFRs
+  real CFRtxR[4] = beta_rng(CFRtxA,CFRtxB);
+  real CFRtxRadj[4] = {alo(lgcfrOR[1],CFRtxR[1]),
+    alo(lgcfrOR[2],CFRtxR[2]),
+    alo(lgcfrOR[3],CFRtxR[3]),
+    alo(lgcfrOR[4],CFRtxR[4])}; //adjusted by ORs
+  row_vector[4] CFRtxV = to_row_vector(CFRtxRadj);
+  matrix[N,4] CFRtx = rep_matrix(CFRtxV,N);
+  //reformed CMRs
+  real CMRtxR[4] = beta_rng(seqA,seqB);
+  real CMRtxRadj[4] = {alo(lgcfrOR[1],CMRtxR[1]),
+    alo(lgcfrOR[2],CMRtxR[2]),
+    alo(lgcfrOR[3],CMRtxR[3]),
+    alo(lgcfrOR[4],CMRtxR[4])}; //adjusted by ORs
+  row_vector[4] CMRtxV = to_row_vector(CMRtxRadj);
+  matrix[N,4] CMRtx = rep_matrix(CMRtxV,N);
+  //calculations
+  matrix<lower=0>[N,4] untreated = fdim(TBMI,TBMnotes); //caps over dx
+  matrix<lower=0>[N,4] deaths = CFRtx .* TBMnotes + untreated;
+  matrix<lower=0>[N,4] morbs = CMRtx .* (1-CFRtx) .* TBMnotes;
+  real global_mnotes = sum(TBMnotes);
+  real global_minc = sum(TBMI);
+  real global_deaths = sum(deaths);
+  real global_morbs = sum(morbs);     
+  //notes
+  real regional_AFR_mnotes = sum(TBMnotes[whoregionAFR,1:4]);
+  real regional_AMR_mnotes = sum(TBMnotes[whoregionAMR,1:4]);
+  real regional_EMR_mnotes = sum(TBMnotes[whoregionEMR,1:4]);
+  real regional_EUR_mnotes = sum(TBMnotes[whoregionEUR,1:4]);
+  real regional_SEA_mnotes = sum(TBMnotes[whoregionSEA,1:4]); 
+  real regional_WPR_mnotes = sum(TBMnotes[whoregionWPR,1:4]);
+  //incidence
+  real regional_AFR_minc = sum(TBMI[whoregionAFR,1:4]);
+  real regional_AMR_minc = sum(TBMI[whoregionAMR,1:4]);
+  real regional_EMR_minc = sum(TBMI[whoregionEMR,1:4]);
+  real regional_EUR_minc = sum(TBMI[whoregionEUR,1:4]);
+  real regional_SEA_minc = sum(TBMI[whoregionSEA,1:4]); 
+  real regional_WPR_minc = sum(TBMI[whoregionWPR,1:4]);
+  //deaths
+  real regional_AFR_deaths = sum(deaths[whoregionAFR,1:4]);
+  real regional_AMR_deaths = sum(deaths[whoregionAMR,1:4]);
+  real regional_EMR_deaths = sum(deaths[whoregionEMR,1:4]);
+  real regional_EUR_deaths = sum(deaths[whoregionEUR,1:4]);
+  real regional_SEA_deaths = sum(deaths[whoregionSEA,1:4]); 
+  real regional_WPR_deaths = sum(deaths[whoregionWPR,1:4]);
+  //morbs
+  real regional_AFR_morbs = sum(morbs[whoregionAFR,1:4]);
+  real regional_AMR_morbs = sum(morbs[whoregionAMR,1:4]);
+  real regional_EMR_morbs = sum(morbs[whoregionEMR,1:4]);
+  real regional_EUR_morbs = sum(morbs[whoregionEUR,1:4]);
+  real regional_SEA_morbs = sum(morbs[whoregionSEA,1:4]); 
+  real regional_WPR_morbs = sum(morbs[whoregionWPR,1:4]);
+  //incidence - by age
+  real age_u1_minc = sum(TBMI[,1]);
+  real age_1to4_minc = sum(TBMI[,2]);
+  real age_5to9_minc = sum(TBMI[,3]);
+  real age_10to14_minc = sum(TBMI[,4]);
+  //deaths - by age
+  real age_u1_deaths = sum(deaths[,1]);
+  real age_1to4_deaths = sum(deaths[,2]);
+  real age_5to9_deaths = sum(deaths[,3]);
+  real age_10to14_deaths = sum(deaths[,4]);
+  //morbs - by age
+  real age_u1_morbs = sum(morbs[,1]);
+  real age_1to4_morbs = sum(morbs[,2]);
+  real age_5to9_morbs = sum(morbs[,3]);
+  real age_10to14_morbs = sum(morbs[,4]);                  
+}
